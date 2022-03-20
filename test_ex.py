708be00191a1c03b8bedeb752c5a97998ed6ff0b
awRosx <hjcrji996@gmail.com>
2022-03-20 13:34:29 +0300

    add new test data

--- test_ex.py
+++ test_ex.py
@@ -1,42 +1,35 @@
+import unittest
 from selenium import webdriver
 import time
-import pytest
 
-class TestAbs(unittest.TestCase):
-    def test_abs1(self):
-        link = "http://suninjuly.github.io/registration1.html"
-        browser = webdriver.Chrome()
+welc = "Congratulations! You have successfully registered!"
+def registration(link):
+    with webdriver.Chrome() as browser:
         browser.get(link)
 
-        element1 = browser.find_elements_by_css_selector("#[placeholder=Input your first name]")
-        element1.send_keys('soos')
+        input1 = browser.find_element_by_css_selector('.first_block .first')
+        input1.send_keys("va")
+        input2 = browser.find_element_by_css_selector('.first_block .second')
+        input2.send_keys("dva")
+        input3 = browser.find_element_by_css_selector(".first_block .third")
+        input3.send_keys("Triee")
+        time.sleep(1)
+
         button = browser.find_element_by_css_selector("button.btn")
         button.click()
         time.sleep(1)
         welcome_text_elt = browser.find_element_by_tag_name("h1")
-        welcome_text = welcome_text_elt.text
-        assert "Congratulations! You have successfully registered!" == welcome_text
-        # ожидание чтобы визуально оценить результаты прохождения скрипта
-        time.sleep(10)
-        # закрываем браузер после всех манипуляций
-        browser.quit()
-    def test_abs2(self):
-        link = "http://suninjuly.github.io/registration2.html"
-        browser = webdriver.Chrome()
-        browser.get(link)
+        return welcome_text_elt.text
 
-        elements = browser.find_elements_by_css_selector("[required]")
-        for element in elements:
-            element.send_keys("Хоба")
-        button = browser.find_element_by_css_selector("button.btn")
-        button.click()
-        time.sleep(1)
-        welcome_text_elt = browser.find_element_by_tag_name("h1")
-        welcome_text = welcome_text_elt.text
-        assert "Congratulations! You have successfully registered!" == welcome_text
-        # ожидание чтобы визуально оценить результаты прохождения скрипта
-        time.sleep(10)
-        # закрываем браузер после всех манипуляций
-        browser.quit()
+class TestsForPages(unittest.TestCase):
+    def test_first_page(self):
+        p1 = registration("http://suninjuly.github.io/registration1.html")
+        self.assertEqual(p1, welc)
+
+    def test_second_page(self):
+        p2 = registration("http://suninjuly.github.io/registration2.html")
+        self.assertEqual(p2, welc)
+
+
 if __name__ == "__main__":
     unittest.main()
