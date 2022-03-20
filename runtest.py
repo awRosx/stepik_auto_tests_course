d6fbef7a3caded5ab170c19d11c7488c7db563f2
awRosx <hjcrji996@gmail.com>
2022-03-20 13:23:07 +0300

    add pytest

--- runtest.py
+++ runtest.py
@@ -1,23 +1,42 @@
 from selenium import webdriver
 import time
+import pytest
 
-try:
-    link = "http://suninjuly.github.io/registration2.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
 
-    elements = browser.find_elements_by_css_selector("[required]")
-    for element in elements:
-        element.send_keys("Хоба")
-    button = browser.find_element_by_css_selector("button.btn")
-    button.click()
-    time.sleep(1)
-    welcome_text_elt = browser.find_element_by_tag_name("h1")
-    welcome_text = welcome_text_elt.text
-    assert "Congratulations! You have successfully registered!" == welcome_text
+        element1 = browser.find_elements_by_css_selector("#[placeholder=Input your first name]")
+        element1.send_keys('soos')
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+        time.sleep(1)
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+        assert "Congratulations! You have successfully registered!" == welcome_text
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+    def test_abs2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
 
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+        elements = browser.find_elements_by_css_selector("[required]")
+        for element in elements:
+            element.send_keys("Хоба")
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+        time.sleep(1)
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+        assert "Congratulations! You have successfully registered!" == welcome_text
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+if __name__ == "__main__":
+    unittest.main()
